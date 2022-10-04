0366e1a5d40e901aba98dd40cfe4924aa2d7a21b
zeretkk <kirill03192@gmail.com>
2022-10-04 20:30:04 +0300

    browser router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { usePlatform, ConfigProvider, AppRoot } from '@vkontakte/vkui';
 import { useState } from 'react';
-import {router} from './routes';
-import { RouterProvider } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.scss';
 import "@vkontakte/vkui/dist/vkui.css";
 import {UserContext} from './context/UserContext'
 import {ToastContext} from "./context/ToastContext";
 import Toast from "./components/Toast";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Profile from "./components/Profile";
 
 function App() {
   let platform = usePlatform()
-  // const navigate = useNavigate()
   const [user, setUser] = useState(null)
   const [token, setToken] = useState(null)
   const [heading, setHeading] = useState(null)
@@ -22,12 +23,16 @@ function App() {
     <ConfigProvider appearance="dark" platform={platform}>
       <AppRoot>
         <UserContext.Provider value={{user, setUser, token, setToken}}>
-          {/* <Main/> */}
           <ToastContext.Provider value={{heading, setHeading, text, setText, toastOpen, setToastOpen}}>
             <Toast/>
-            <RouterProvider router={router}/>
+            <BrowserRouter>
+              <Routes>
+                <Route path={'/'} element={<Main/>}/>
+                <Route path={'/load'} element={<Loader/>}/>
+                <Route path={'/profile'} element={<Profile/>}/>
+              </Routes>
+            </BrowserRouter>
           </ToastContext.Provider>
-
         </UserContext.Provider>
       </AppRoot>
     </ConfigProvider>
