45782b351cd42d689942f96beac4169e6493ab58
zeretkk <kirill03192@gmail.com>
2022-10-04 18:16:39 +0300

    toast auto hiding

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,9 +1,15 @@
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import {ToastContext} from "../context/ToastContext";
 import {RiNotificationBadgeFill} from 'react-icons/ri'
 
 function Toast() {
-    const {heading, text, toastOpen} = useContext(ToastContext)
+    const {heading, text, toastOpen, setToastOpen} = useContext(ToastContext)
+    useEffect(()=>{
+        const timer = setTimeout(()=>{
+            setToastOpen(false)
+        }, 5000)
+        return  ()=> clearTimeout(timer)
+    }, [toastOpen])
     return (
         <div className={`toast ${toastOpen?'':'d-none'}`}>
             <div className="toast__header">
